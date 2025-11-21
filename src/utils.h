3b9dfb6bbb2de26aa83d43b9bfd6ae2be3958d3d
kimNewpsysk1t <kim86226663@gmail.com>
2025-11-21 18:40:11 +0800

    Update utils.h

--- src/utils.h
+++ src/utils.h
@@ -1,9 +1,15 @@
-#ifndef UTILS_H
+#ifndef INPUT_H		
 #define UTILS_H
 
 #include <stdbool.h>
 
 // hy proto
+void clean_input(char str[]);
+bool is_valid_name(const char* s);
+bool read_line(char* buffer, int size);
+bool read_optional_string(char* buffer, int size, const char* prompt);
+bool read_optional_mark(float* result, const char* prompt);
+
 
 // function prototypes
 void clean_input(char command[]);
